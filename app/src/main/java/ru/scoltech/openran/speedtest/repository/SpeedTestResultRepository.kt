e0cebd0e0170f56a467bd9375aa0d0f6e3a52593
Kazakov Mihail <advancerman@gmail.com>
2021-08-21 23:26:20 +0700

    Add `SpeedTestResultRepository.getInstance`
    Add convenient function for SpeedTestResultRepository using as singleton

--- app/src/main/java/ru/scoltech/openran/speedtest/repository/SpeedTestResultRepository.kt
+++ app/src/main/java/ru/scoltech/openran/speedtest/repository/SpeedTestResultRepository.kt
@@ -1,9 +1,30 @@
 package ru.scoltech.openran.speedtest.repository
 
+import android.content.Context
+import android.database.SQLException
+import android.database.sqlite.SQLiteException
 import ru.scoltech.openran.speedtest.domain.SpeedTestResult
+import kotlin.jvm.Throws
 
 interface SpeedTestResultRepository {
     fun save(result: SpeedTestResult)
     fun findAllByPageAndSizeOrderedById(page: Long, size: Long): List<SpeedTestResult>
     fun findAll(): List<SpeedTestResult>
+
+    companion object {
+        @Volatile
+        private lateinit var INSTANCE: SpeedTestResultRepository
+
+        @Throws(SQLException::class, SQLiteException::class)
+        fun getInstance(context: Context): SpeedTestResultRepository {
+            if (!this::INSTANCE.isInitialized) {
+                synchronized(this) {
+                    if (!this::INSTANCE.isInitialized) {
+                        INSTANCE = SpeedTestResultRepositoryImpl(context)
+                    }
+                }
+            }
+            return INSTANCE
+        }
+    }
 }
