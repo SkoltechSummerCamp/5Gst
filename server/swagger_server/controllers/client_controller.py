283805647fd8cc5649aa37a13ab06d7e1f271ec8
Eugene Mavrin <eugene.mavrin@gmail.com>
2021-08-30 13:07:06 +0300

    Update client_controller.py

--- server/swagger_server/controllers/client_controller.py
+++ server/swagger_server/controllers/client_controller.py
@@ -18,6 +18,6 @@ def client_obtain_ip():  # noqa: E501
     """
     try:
         ((ip, port), (time, port_iperf)) = ServerDictInst.get()
-        return ServerAddr(ip=ip, port=port, port_iperf=port_iperf, time=time), 200
+        return [ServerAddr(ip=ip, port=port, port_iperf=port_iperf)], 200
     except:
-        return {}, 503
+        return [], 503
