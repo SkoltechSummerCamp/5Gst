7f98a96b18049ee7dc5fccaac2e2902353307733
Andrey Semenov <blizkrig@gmail.com>
2021-09-20 15:11:47 +0300

    UTIL: update codestyle, Move all constants to static members

--- app/src/main/java/ru/scoltech/openran/speedtest/Wave.java
+++ app/src/main/java/ru/scoltech/openran/speedtest/Wave.java
@@ -9,7 +9,16 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 public class Wave extends View {
-
+    private static final int mAlphaChanelValue = 180;
+    private static final int mRedChanelValue = 3;
+    private static final int mGreenChanelValue = 218;
+    private static final int mMinimumGreenChanelValue = 63;
+    private static final int mBlueChanelValue = 197;
+    private static final int mWaveAplitudeDevider = 20;
+    private static final float mFirstWaveFrequency = 1.2f;
+    private static final float mSecondWaveFrequency = 1.0f;
+    private static final int mSecondWaveOffset = 15;
+    private static final float mOffsetAddendum = 1.0f;
     private Paint mPaint;
     private int mCurrentSpeed;
     private float mOffsetX;
@@ -24,7 +33,8 @@ public class Wave extends View {
         path.moveTo(width,height);
         path.lineTo(0, height);
         for (int x = 0; x <= width; x++) {
-            path.lineTo(x, height/8 -  amplitude* (float) Math.sin(frequency*4.0 * Math.PI * (x + xOffset) / width));
+            path.lineTo(x, (float) height / 8 - amplitude * 
+                    (float) Math.sin(frequency * 4.0 * Math.PI * (x + xOffset) / width));
         }
         path.close();
         return path;
@@ -34,24 +44,24 @@ public class Wave extends View {
         mPaint.setStrokeWidth(1);
         mPaint.setAntiAlias(true);
         mPaint.setStyle(Paint.Style.FILL);
-        mPaint.setARGB(30,101,101,101);
+        mPaint.setARGB(mAlphaChanelValue, mRedChanelValue, mGreenChanelValue, mBlueChanelValue);
         mCurrentSpeed = 0;
     }
 
     @Override
     public void onDraw(Canvas canvas) {
-        int width, height;
-        width = getWidth();
-        height = getHeight();
-        int colorValue = 63+ (mCurrentSpeed > 155 ? 155 : mCurrentSpeed);
-        Path p = buildWavePath(width*2, height+height,
-                1.2f, mOffsetX,height/20);
-        Path p2 = buildWavePath(width*2, height+height,
-                1.0f, mOffsetX+15,height/20);
-        mPaint.setARGB(180,3,colorValue,197);
+        int width = getWidth();
+        int height = getHeight();
+        int colorValue = Math.min(mGreenChanelValue,
+                mMinimumGreenChanelValue + mCurrentSpeed);
+        Path p = buildWavePath(width * 2, height * 2,
+                mFirstWaveFrequency, mOffsetX, height / mWaveAplitudeDevider);
+        Path p2 = buildWavePath(width * 2, height * 2,
+                mSecondWaveFrequency, mOffsetX + mSecondWaveOffset, height / mWaveAplitudeDevider);
+        mPaint.setARGB(mAlphaChanelValue, mRedChanelValue, colorValue, mBlueChanelValue);
         canvas.drawPath(p, mPaint);
         canvas.drawPath(p2, mPaint);
-        mOffsetX += 35;
+        mOffsetX += mOffsetAddendum;
     }
 
     public void attachSpeed(int speed) { // attach current instant speed to wave
