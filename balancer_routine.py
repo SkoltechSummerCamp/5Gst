00233873688f220b16dce4387603973fa6554454
Kazakov Mihail <advancerman@gmail.com>
2022-07-05 23:11:11 +0300

    Update balancer api calls

--- balancer_routine.py
+++ balancer_routine.py
@@ -9,21 +9,21 @@ import sys
 class BalancerRoutine:
     def __init__(self):
         # create an instance of the API class
-        self.api_instance = swagger_client.ServerApi()
+        self.api_instance = swagger_client.ServiceApi()
 
     def post_to_server(self, port=5000, port_iperf=5001):
-        body = swagger_client.ServerAddr(port=port, port_iperf=port_iperf) # ServerAddr | port of iperf server. Ip and time could be emply (optional)
+        body = swagger_client.ServerAddressRequest(ip=self.env_data['SERVICE_IP_ADDRESS'], port=port, port_iperf=port_iperf) # ServerAddr | port of iperf server. Ip and time could be emply (optional)
         try:
             # post self ip to balancer
-            self.api_instance.server_post_ip(body=body)
+            self.api_instance.service_create(body=body)
         except ApiException as e:
             print("Exception when calling ServerApi->server_post_ip: %s\n" % e)
 
     def delete_from_server(self, port=5000, port_iperf=5001):
-        body = swagger_client.ServerAddr(port=port, port_iperf=port_iperf) # ServerAddr | port of iperf server. Ip and time could be emply (optional)
+        body = swagger_client.ServerAddressRequest(ip=self.env_data['SERVICE_IP_ADDRESS'], port=port, port_iperf=port_iperf) # ServerAddr | port of iperf server. Ip and time could be emply (optional)
         try:
             # delete server IP
-            api_response = self.api_instance.server_delete_ip(body=body)
+            api_response = self.api_instance.service_delete(body=body)
             pprint(api_response)
         except ApiException as e:
             print("Exception when calling ServerApi->server_delete_ip: %s\n" % e)
