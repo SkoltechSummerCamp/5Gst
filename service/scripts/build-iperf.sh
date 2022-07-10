adaf84b3d793579ffab1bb4c135dfa2077b78ed6
Kazakov Mihail <advancerman@gmail.com>
2022-07-10 16:27:18 +0300

    Update service code to fit monorepo
    Signed-off-by: Kazakov Mihail <advancerman@gmail.com>

--- service/scripts/build-iperf.sh
+++ service/scripts/build-iperf.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-cd iPerf
+cd ../iPerf
 head -1 src/Launch.cpp | grep vinogradov.alek@gmail.com
 #If the patch has not been applied then the $? which is the exit status 
 #for last command would have a success status code = 0
@@ -7,12 +7,12 @@ if [ $? -eq 1 ];
 then
     #apply the patch
     echo "patching iPerf"
-    patch src/Launch.cpp < ../scripts/iPerf_patch.diff
+    patch src/Launch.cpp < ../service/scripts/iPerf_patch.diff
 else
     echo "already pathed"
 fi
 bash configure
 make
-cd ..
-cp iPerf/src/iperf iperf.elf
+cd ../service
+cp ../iPerf/src/iperf iperf.elf
 
