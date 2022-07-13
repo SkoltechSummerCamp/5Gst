d02cf024c71213c829c7ad5d719bef1c49c2b8e5
Kazakov Mihail <advancerman@gmail.com>
2022-07-13 21:21:04 +0300

    Update workflows
    Signed-off-by: Kazakov Mihail <advancerman@gmail.com>

--- service/scripts/build-iperf.sh
+++ service/scripts/build-iperf.sh
@@ -1,16 +1,5 @@
 #/bin/sh
 cd ../iPerf
-head -1 src/Launch.cpp | grep vinogradov.alek@gmail.com
-#If the patch has not been applied then the $? which is the exit status
-#for last command would have a success status code = 0
-if [ $? -eq 1 ];
-then
-    #apply the patch
-    echo "patching iPerf"
-    patch src/Launch.cpp < ../service/scripts/iPerf_patch.diff
-else
-    echo "already pathed"
-fi
 bash configure
 make
 cd ../service
