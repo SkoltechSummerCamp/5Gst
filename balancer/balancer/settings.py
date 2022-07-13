b287d1997fa494e99a05deaaa60444c7e7c6db58
Andy <56515784+AndyForWork@users.noreply.github.com>
2022-07-13 15:56:22 +0300

    added db connection (#19)
    * added db connection
    
    * Update settings.py

--- balancer/balancer/settings.py
+++ balancer/balancer/settings.py
@@ -66,10 +66,15 @@ WSGI_APPLICATION = 'balancer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
