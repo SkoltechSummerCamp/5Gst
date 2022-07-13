36ee083545fffb3479a4105aa4f76f23301833d3
Andy <56515784+AndyForWork@users.noreply.github.com>
2022-07-13 15:35:58 +0300

    Update settings.py

--- balancer/balancer/settings.py
+++ balancer/balancer/settings.py
@@ -68,9 +68,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['ROLE'],
-        'PASSWORD': os.environ['PASS'],
-        'HOST': os.environ['DB_IP'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
         'PORT': os.environ['DB_PORT'],
     }
 }
