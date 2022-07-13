47b27fda4536112fd8966c684aedc91c89e953b5
Klanick <niklavr019@gmail.com>
2022-07-13 19:00:07 +0300

    Deduplication update

--- balancer/services/serializers.py
+++ balancer/services/serializers.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.core import validators
 from rest_framework import serializers
 
 import services.models as models
@@ -11,10 +12,30 @@ class ServerAddressResponseSerializer(serializers.ModelSerializer):
         fields = ('ip', 'port', 'port_iperf', 'time')
 
 
-class ServerAddressRequestSerializer(serializers.ModelSerializer):
+class ServerAddressRequestSerializer(serializers.Serializer):
+    ip = serializers.IPAddressField()
+    port = serializers.IntegerField(
+        validators=[
+            validators.MinValueValidator(0),
+            validators.MaxValueValidator(65535),
+        ]
+    )
+
+    port_iperf = serializers.IntegerField(
+        validators=[
+            validators.MinValueValidator(0),
+            validators.MaxValueValidator(65535),
+        ]
+    )
+
     def validate(self, attrs):
         return {'time': datetime.datetime.utcnow(), **attrs}
 
-    class Meta:
-        model = models.ServerAddress
-        fields = ('ip', 'port', 'port_iperf')
+    def create(self, validated_data):
+        return models.ServerAddress.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        pass
+
+    def save(self, **kwargs):
+        models.ServerAddress.objects.update_or_create(**self.data, defaults=self.validate(self.data))
