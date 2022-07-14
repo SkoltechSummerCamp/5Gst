83b592316dd142a0c5aa22ef5554c5d5bd1cf164
Kazakov Mihail <advancerman@gmail.com>
2022-07-14 22:04:46 +0300

    Fix service docker build

--- service/docker_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp -r ../iPerf .
-docker build -t docker_service .
-rm -rdf ./iPerf
--- service/scripts/build-iperf.sh
+++ service/scripts/build-iperf.sh
@@ -1,6 +1,6 @@
 #/bin/sh
-cd ../iPerf
+cd ./iPerf
 bash configure
 make
-cd ../service
-cp ../iPerf/src/iperf iperf.elf
+cd ..
+cp ./iPerf/src/iperf iperf.elf
