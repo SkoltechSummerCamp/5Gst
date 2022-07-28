cd6b0ec2f482979dca6189f0550637981aa5ad1d
AndyForWork <andreysannikow@gmail.com>
2022-07-28 13:56:52 +0300

    Revert "Update tests.py"
    This reverts commit d12408a186e7c78b83a2a00a1a8228fe7681a6da.

--- service/apps/tests.py
+++ service/apps/tests.py
@@ -1,6 +1,6 @@
 from rest_framework.test import APITestCase
 
 
-class ServiceTestCase(APITestCase):
-    def test_startup(self):
+class ServiceTestsCase(APITestCase):
+    def some_tests(self):
         pass
