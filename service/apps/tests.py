d12408a186e7c78b83a2a00a1a8228fe7681a6da
Andy <56515784+AndyForWork@users.noreply.github.com>
2022-07-28 13:29:52 +0300

    Update tests.py

--- service/apps/tests.py
+++ service/apps/tests.py
@@ -1,6 +1,6 @@
 from rest_framework.test import APITestCase
 
 
-class ServiceTestsCase(APITestCase):
-    def some_tests(self):
+class ServiceTestCase(APITestCase):
+    def test_startup(self):
         pass
