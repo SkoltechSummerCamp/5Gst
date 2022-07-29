7d1c4116ca632d22f0f73a8439ac0adefd72c3b8
Andy <56515784+AndyForWork@users.noreply.github.com>
2022-07-29 17:51:06 +0300

    Update docker_unpack.sh

--- docker_unpack.sh
+++ docker_unpack.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-mkdir 5GST
-cd 5GST
-
 echo "load images from .tar"
 docker load -i service.tar 
 docker load -i balancer.tar 
 docker load -i postgres.tar 
 
-cd $OLDPWD
-rm -r 5GST
 echo "all images downladed to your usb drive"
